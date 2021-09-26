d4de11adce681048a2ee97f69fc352a4336c5ea8
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-09-26 09:49:00 +0900

    remove

--- src/tools/createListData.ts
+++
@@ -1,9 +0,0 @@
-const list: string[] = [];
-
-for (const entry of Deno.readDirSync("./docs/data")) {
-  list.push(entry.name.replace(".json", ""));
-}
-
-Deno.writeTextFileSync("./docs/list.json", JSON.stringify(list, null, "\t"));
-
-console.log("[SUCCESS]");
