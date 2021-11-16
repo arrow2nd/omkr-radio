90fb9f3281773e3993b6acbaeac9809ff296c1c6
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-16 11:26:23 +0900

    fix: no audio file

--- src/util/fetchRadioUrl.ts
+++ src/util/fetchRadioUrl.ts
@@ -4,13 +4,15 @@
  * @param url 記事のURL
  * @returns 音源ファイルまでのパス（radio 以下）
  */
-export async function fetchRadioFilePath(url: string): Promise<string> {
+export async function fetchRadioFilePath(
+  url: string
+): Promise<string | undefined> {
   const res = await fetch(url);
   const html = await res.text();
 
   const matched = html.match(
-    /https:\/\/omocoro\.heteml\.net\/radio\/(.*?\.mp3)/,
+    /https:\/\/omocoro\.heteml\.net\/radio\/(.*?\.mp3)/
   );
 
-  return matched ? matched[1] : "";
+  return matched ? matched[1] : undefined;
 }
