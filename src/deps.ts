c3a64abaa3c784416f24e87cfda03a2a73b2d140
arrow2nd <arrow2nd@users.noreply.github.com>
2022-07-25 15:03:05 +0000

    chore(deps): 依存関係を更新

--- src/deps.ts
+++ src/deps.ts
@@ -2,6 +2,6 @@ import ky from "https://cdn.skypack.dev/ky?dts";
 
 export { ky };
 export { parseFeed } from "https://deno.land/x/rss@0.5.6/mod.ts";
-export { DOMParser } from "https://deno.land/x/deno_dom@v0.1.32-alpha/deno-dom-wasm.ts";
+export { DOMParser } from "https://deno.land/x/deno_dom@v0.1.33-alpha/deno-dom-wasm.ts";
 export { crypto } from "https://deno.land/std@0.149.0/crypto/mod.ts";
 export { parse, stringify } from "https://deno.land/x/xml@2.0.4/mod.ts";
